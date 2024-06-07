32d944c0a7459dca57ff0ede11ecd7afaa002997
Don <120154112+zen-da-baker@users.noreply.github.com>
2024-06-07 14:34:37 -0700

    Header file created

---
+++ functions.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+void display_grid(grid_column1, grid_column2, grid_column3, turn) {
+    std::cout << "Turn: " << turn << "\n";
+    std::cout << grid_column1 << "\n";
+    std::cout << grid_column2 << "\n";
+    std::cout << grid_column3 << "\n";
+}
